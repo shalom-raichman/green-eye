1daeaf7dae4727097145356818734644de7bc3bb
Shalom Raichman <shalomr1770@gmail.com>
2024-10-07 10:16:15 +0300

    basic index file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log("is running 😝");
+const exp = require("express")
+const dotenv = require("dotenv")
+
+const app = exp()
+dotenv.config()
+const PORT = process.env.PORT || 1415
+
+app.listen(PORT, ()=>console.log(`Server is up and running on port ${PORT}`))
