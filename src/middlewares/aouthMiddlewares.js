70bc336b96e0cee4c63cbeb5847cf09648b402b0
Shalom Raichman <shalomr1770@gmail.com>
2024-10-07 11:42:10 +0300

    empty middlewares

---
+++ src/middlewares/aouthMiddlewares.js
@@ -0,0 +1,14 @@
+const onlyCommanders = async (req, res, next) => {
+    try {
+    } catch (error) {}
+};
+
+const onlySoldiersAndCommanders = async (req, res, next) => {
+    try {
+    } catch (error) {}
+};
+
+module.exports = {
+    onlyCommanders,
+    onlySoldiersAndCommanders,
+};
