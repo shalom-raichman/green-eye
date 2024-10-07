83edaa1ac8b94d70bd0854fd5080d581ca62e7ee
Shalom Raichman <shalomr1770@gmail.com>
2024-10-07 17:09:09 +0300

    mw done

--- src/middlewares/aouthMiddlewares.js
+++ src/middlewares/aouthMiddlewares.js
@@ -1,25 +1,41 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 const onlyCommanders = async (req, res, next) => {
     try {
-    } catch (err) {}
+        // get the token from cookie
+        const token = req.cookies.token;
+        // verify
+        const userData = await jwt.verify(token, process.env.TOKEN_SECRET);
+        if (userData.role != "commander") {
+            res.status(403).json({ msg: "shtzchhhhhhhhh...." });
+        }
+        // add the user to the req obj
+        req.user = userData;
+        // call next
+        next();
+    } catch (err) {
+        console.log(err);
+        res.status(401).json({
+            msg: "not aloud",
+        });
+    }
 };
 
 const onlySoldiersAndCommanders = async (req, res, next) => {
     try {
         // get the token from cookie
-        const token = req.cookies.token
+        const token = req.cookies.token;
         // verify
-        const userData = await jwt.verify(token, process.env.TOKEN_SECRET)
+        const userData = await jwt.verify(token, process.env.TOKEN_SECRET);
         // add the user to the req obj
-        req.user = userData
+        req.user = userData;
         // call next
-        next()
+        next();
     } catch (err) {
         console.log(err);
         res.status(401).json({
-            msg: "not aloud"
-        })
+            msg: "not aloud",
+        });
     }
 };
 
