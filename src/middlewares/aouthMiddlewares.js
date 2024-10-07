00edbbfd0868b5a11a5a2e3cb55783af7190e4b7
Shalom Raichman <shalomr1770@gmail.com>
2024-10-07 17:00:59 +0300

    add first middleware

--- src/middlewares/aouthMiddlewares.js
+++ src/middlewares/aouthMiddlewares.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken")
+
 const onlyCommanders = async (req, res, next) => {
     try {
     } catch (err) {}
@@ -5,7 +7,20 @@ const onlyCommanders = async (req, res, next) => {
 
 const onlySoldiersAndCommanders = async (req, res, next) => {
     try {
-    } catch (err) {}
+        // get the token from cookie
+        const token = req.cookies.token
+        // verify
+        const userData = await jwt.verify(token, process.env.TOKEN_SECRET)
+        // add the user to the req obj
+        req.user = userData
+        // call next
+        next()
+    } catch (err) {
+        console.log(err);
+        res.status(401).json({
+            msg: "not aloud"
+        })
+    }
 };
 
 module.exports = {
