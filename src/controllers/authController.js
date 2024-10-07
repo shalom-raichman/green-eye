88490859d12d353429a6d2e51c15734f3f6cdb9e
Shalom Raichman <shalomr1770@gmail.com>
2024-10-07 16:49:33 +0300

    register and auth done

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,16 @@
+const {login: loginFromSrvice} = require("../sevices/authService")
+
 const login = async (req, res) => {
     try {
-    } catch (err) {}
+        const token = await loginFromSrvice(req.body)
+        res.cookie("token", token)
+        res.json({
+            msg: `Welcome ${req.body.user_name}! so good to se you!!`
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err)
+    }
 };
 
 const logout = async (req, res) => {
