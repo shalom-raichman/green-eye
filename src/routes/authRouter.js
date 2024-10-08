7a4f2c0f5afca5b07052f638a093839ca69659f0
Shalom Raichman <shalomr1770@gmail.com>
2024-10-08 15:28:39 +0300

    swagger documitation for login created

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -4,17 +4,39 @@ const {
     onlySoldiersAndCommanders,
 } = require("../middlewares/aouthMiddlewares");
 
-// routes/authRouter.js
 /**
  * @swagger
- * /login:
+ * /auth/login:
  *   post:
- *     summary: Returns a sample message
+ *     summary: Login to the system.
+ *     description: Login to the system.
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             user:
+ *               type: object
+ *               properties:
+ *                 user_name:
+ *                   type: string
+ *                   description: no description
+ *                 password:
+ *                   type: string
+ *                   description: no description
+ *     example:
+ *         id: d5fE_asz
+ *         title: The New Turing Omnibus
+ *         author: Alexander K. Dewdney
+ *         finished: false
+ *         createdAt: 2020-03-10T04:05:06.157Z
  *     responses:
- *       200:
+ *       '201':
  *         description: A successful response
+ *       '404':
+ *         description: Employee not found
+ *       '500':
+ *         description: Internal server error
  */
-
 router.post("/login", login);
 
 router.post("/logout", onlySoldiersAndCommanders, logout);
